92a78e4ec90ec4d03efd61436d435719ac53cb8e
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-03-09 00:52:15 +0530

    Time: 130 ms (70.13%), Space: 14.4 MB (61.59%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,47 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        m=len(grid)
+        n=len(grid[0])
+
+        def valid(i,j):
+            if i<0 or i>=m:
+                return False
+            if j<0 or j>=n:
+                return False
+            if grid[i][j] == 1:
+                return False
+            return True
+
+        def dfs(i,j):
+            grid[i][j] = 1
+
+            if valid(i+1,j):
+                dfs(i+1,j)
+            if valid(i-1,j):
+                dfs(i-1,j)
+            if valid(i,j+1):
+                dfs(i,j+1)
+            if valid(i,j-1):
+                dfs(i,j-1)
+
+        
+        for i in range(m):
+            for j in range(n):
+                if i*j ==0 or i==m-1 or j==n-1:
+                    if grid[i][j] == 0:
+                        dfs(i,j)
+
+        count=0
+        for i in range(1,m-1):
+            for j in range(1,n-1):
+                if grid[i][j] == 0:
+                    count+=1
+                    dfs(i,j)
+                        
+
+        return count
+
+
+            
+            
+            

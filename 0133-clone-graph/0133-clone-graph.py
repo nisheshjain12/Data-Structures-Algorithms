95f5330447796d39355e17c507d2864d4c9cea78
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-03-10 00:48:49 +0530

    Time: 39 ms (73.27%), Space: 14.3 MB (66.90%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        map={}
+        
+        def dfs(node):
+            if node in map:
+                return map[node]
+            copy=Node(node.val)
+            map[node] = copy
+
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+            return copy
+        return dfs(node)

9b0c511e08f04bbf7dc38536fba85491b3aa7701
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-03-19 14:58:05 +0530

    Time: 32 ms (71.93%), Space: 13.8 MB (95.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[]
-        dp.append(nums[0])
+        pre2=0
+        pre1=nums[0]
         
         for i in range(1,n):
             pick=nums[i]
             if i>1:
-                pick+=dp[i-2]
-            notpick=dp[i-1]
-            dp.append(max(pick,notpick))
+                pick+=pre2
+            notpick=pre1
+            curr=max(pick,notpick)
+            pre2=pre1
+            pre1=curr
         
-        return dp[n-1]
+        return pre1

89b36ca95821150ec5de26c8e2d09d2755523a1d
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-06 13:11:14 +0530

    Time: 129 ms (66.05%), Space: 14.5 MB (61.65%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -3,26 +3,24 @@ class Solution:
         m=len(grid)
         n=len(grid[0])
 
-        def valid(i,j):
-            if i<0 or i>=m:
-                return False
-            if j<0 or j>=n:
-                return False
-            if grid[i][j] == 1:
-                return False
-            return True
+        # def valid(i,j):
+        #     if i<0 or i>=m:
+        #         return False
+        #     if j<0 or j>=n:
+        #         return False
+        #     if grid[i][j] == 1:
+        #         return False
+        #     return True
 
         def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == 1:
+                return
             grid[i][j] = 1
 
-            if valid(i+1,j):
-                dfs(i+1,j)
-            if valid(i-1,j):
-                dfs(i-1,j)
-            if valid(i,j+1):
-                dfs(i,j+1)
-            if valid(i,j-1):
-                dfs(i,j-1)
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
 
         
         for i in range(m):

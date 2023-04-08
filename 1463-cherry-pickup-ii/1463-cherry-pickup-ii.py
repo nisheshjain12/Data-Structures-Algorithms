9d71377dfd7d822688ee0a4d1b6439958e4d47ad
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-08 18:41:17 +0530

    Time: 4911 ms (6.27%), Space: 21.8 MB (61.54%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -4,29 +4,35 @@ class Solution:
         n=len(grid[0])
         dp=[[[-1 for _ in range(n)] for _ in range(n)] for _ in range(m)]
         
-        def rec(i,j1,j2):
-            if j1<0 or j2<0 or j1>=n or j2>=n:
-                return -10**8
-            if i==m-1:
+
+        # Base case
+        for j1 in range(n):
+            for j2 in range(n):
                 if j1==j2:
-                    return grid[i][j1]
+                    dp[m-1][j1][j2] = grid[m-1][j1]
                 else:
-                    return grid[i][j1]+grid[i][j2]
-            
-            if dp[i][j1][j2] != -1:
-                return dp[i][j1][j2]
-            maxi=-10**8
-            
-            for dj1 in range(-1,2,1):
-                for dj2 in range(-1,2,1):
-                    val=grid[i][j1]
-                    if j1 != j2:
-                        val+=grid[i][j2]
-                        
-                    val+=rec(i+1,j1+dj1,j2+dj2)
-                    maxi = max(maxi,val)
-            dp[i][j1][j2] = maxi
-            
-            return dp[i][j1][j2]           
+                    dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2]
+        
+        
+        for i in range(m-2,-1,-1):
+            for j1 in range(n):
+                for j2 in range(n):
                     
-        return rec(0,0,n-1)
+                    # DP copy 
+                    maxi=-10**8
+                    for dj1 in range(-1,2,1):
+                        for dj2 in range(-1,2,1):
+                            val=grid[i][j1]
+                            if j1 != j2:
+                                val+=grid[i][j2]
+                            if j1+dj1>=0 and j1+dj1<n and j2+dj2>=0 and j2+dj2<n:
+                                val+=dp[i+1][j1+dj1][j2+dj2]
+                            else:
+                                val=-10**8
+                            
+                            maxi = max(maxi,val)
+                            
+                    dp[i][j1][j2] = maxi
+                    
+        return dp[0][0][n-1]
+       

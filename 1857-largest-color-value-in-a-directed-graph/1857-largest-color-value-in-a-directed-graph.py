41e1a6fe095c5c93e16d33be72e7563bdc1ff171
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-10 03:29:45 +0530

    Time: 2389 ms (68.48%), Space: 100.1 MB (59.39%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,35 @@
+# class Solution:
+#     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+#         c=len(colors)
+#         adj = defaultdict(list)
+#         for a,b in edges:
+#             adj[a].append(b)
+        
+#         def dfs(node):
+            
+        
+            
+        
+        
+#         vis=set()
+#         dfsvis=set()
+#         dp=[[-1 for _ in range(n)]for _ in range(c)]
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        
+        N = len(colors)                                     
+        incoming, Graph = [0]*N, [[] for _ in range(N)]       
+        for _, v in edges: incoming[v]+=1                 
+        for u, v in edges: Graph[u].append(v)              
+        stack = [u for u in range(N) if incoming[u]==0]   
+        cnts = [[0]*26 for _ in range(N)]                    
+        while stack:                                        
+            u = stack.pop()                                 
+            cnts[u][ord(colors[u])-ord('a')] += 1           
+            for v in Graph[u]:                               
+                cnts[v] = list(map(max, cnts[v], cnts[u]))   
+                incoming[v] -= 1                             
+                if incoming[v]==0: stack.append(v)           
+
+        return -1 if sum(incoming)>0 else max([max(node) for node in cnts])

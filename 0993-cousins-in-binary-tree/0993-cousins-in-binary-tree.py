fbc62732588164a3230e168a827753708ef7eced
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-19 01:32:43 +0530

    Time: 41 ms (13.51%), Space: 13.9 MB (82.19%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -1,40 +1,26 @@
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         if not root:
-            return False
-        q=[]
-        q.append(root)
-        xP=None
-        yP=None
+            return True
+        xp,xd=None,-1
+        yp,yd=None,-2
         
-        while q:
-            level = len(q)
-            for i in range(level):
-                temp = q.pop(0)
-                
-                if temp.left:
-                    q.append(temp.left)
-                    
-                    if temp.left.val == x:
-                        xP=temp
-                    if temp.left.val == y:
-                        yP=temp
-                        
-                if temp.right:
-                    q.append(temp.right)
-                    
-                    if temp.right.val == x:
-                        xP=temp
-                    if temp.right.val == y:
-                        yP=temp
-                
-                if xP and yP:
-                    if xP!=yP:
-                        return True
-                    else:
-                        return False
-                
-            if (xP and yP==None) or (yP and xP==None):
-                return False
+        def dfs(node,depth,parent):
+            nonlocal xp,xd,yp,yd
+            if not node:
+                return 
+            elif node.val == x:
+                xp=parent
+                xd=depth
+            elif node.val == y:
+                yp=parent
+                yd=depth
+            else:
+                dfs(node.right,depth+1,node)
+                dfs(node.left,depth+1,node)
             
+        dfs(root,0,None)
+        print(xp, yp, xd,yd)
+        if xp!=yp and xd==yd:
+            return True
         return False

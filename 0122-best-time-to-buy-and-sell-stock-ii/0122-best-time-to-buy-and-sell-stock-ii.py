183b7e62416b6cb561b88c9aa49022f3c4c45cac
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-20 04:10:15 +0530

    Time: 83 ms (8.61%), Space: 17.5 MB (5.02%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,19 +2,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         profit=0
-        dp=[[-1 for _ in range(2)] for i in range(n)]
-        def recc(i,buy):
-            if i==n:
-                return 0
-            if dp[i][buy] != -1:
-                return dp[i][buy]
+        dp=[[-1 for _ in range(2)] for i in range(n+1)]
+    
+        dp[n][0] = dp[n][1] = 0
+        
+        for i in range(n-1,-1,-1):
+            for b in range(2):
+                if b:
+                    profit = max(-prices[i] + dp[i+1][0] ,  dp[i+1][1])
+                else:
+                    profit = max(prices[i] +  dp[i+1][1] ,  dp[i+1][0])
+                dp[i][b] = profit
+        
+        return dp[0][1]
                 
-            if buy:
-                profit = max(-prices[i] + recc(i+1,0) , recc(i+1,1))
-            else:
-                profit = max(prices[i] + recc(i+1,1) , recc(i+1,0))
-            dp[i][buy] = profit
-            return profit
-        
-        return recc(0,1)
-        

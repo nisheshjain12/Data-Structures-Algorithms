3a473d305a0638dbcbb265737ae942ba2c817109
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-04-20 03:38:27 +0530

    Time: 119 ms (5.00%), Space: 71.8 MB (5.02%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        profit=0
+        dp=[[-1 for _ in range(2)] for i in range(n)]
+        def recc(i,buy):
+            if i==n:
+                return 0
+            if dp[i][buy] != -1:
+                return dp[i][buy]
+                
+            if buy:
+                profit = max(-prices[i] + recc(i,0) , recc(i+1,1))
+            else:
+                profit = max(prices[i] + recc(i+1,1) , recc(i+1,0))
+            dp[i][buy] = profit
+            return profit
+        
+        return recc(0,1)
+        

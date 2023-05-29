539591b40a238fe60138fa8ba1b2ecd657dee8c9
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-05-29 10:38:50 +0530

    Added solution - LeetHub

--- Reorganize The Array - GFG/reorganize-the-array.py
+++ Reorganize The Array - GFG/reorganize-the-array.py
@@ -1,14 +1,21 @@
 #User function Template for python3
 
 def Rearrange (arr,  n) : 
-    ans=[-1 for _ in range(n)]
+    
     for i in range(n):
-        if arr[i]!=-1:
-            ans[arr[i]] = arr[i]
-    return ans
+        if arr[i]!=-1 and  arr[i]!=i:
+            x = arr[i]
             
-
-
+            while arr[x]!=-1 and arr[x] != x:
+                y=arr[x]
+                arr[x] = x
+                x=y
+            arr[x]  = x
+            if arr[i] !=i:
+                arr[i] = -1
+            
+    return arr
+            
 
 #{ 
  # Driver Code Starts

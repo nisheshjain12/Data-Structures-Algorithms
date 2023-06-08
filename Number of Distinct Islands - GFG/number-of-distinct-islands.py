e4f1b97c13badf011a69a2e7377976bf1f1e48d0
Nishesh Jain <89805538+nisheshjain12@users.noreply.github.com>
2023-06-08 16:02:51 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.py
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.py
@@ -3,53 +3,23 @@
 import sys
 from typing import List
 sys.setrecursionlimit(10**8)
-# class Solution:
-#     def countDistinctIslands(self, grid : List[List[int]]) -> int:
-#         m=len(grid)
-#         n=len(grid[0])
-        
-#         def valid(i,j):
-#             if i>=0 and i<m and j>=0 and j<n and grid[i][j] == 1:
-#                 return True
-#             return False
-            
-#         row=[-1,0,1,0]
-#         col=[0,1,0,-1]
-#         ans=set()
-        
-#         def dfs(i,j,br,bc,v):
-#             v.append((i-br,j-bc))
-            
-#             for i in range(4):
-#                 nr=i+row[i]
-#                 nc=j+col[i]
-
-#                 if valid(nr,nc):
-#                     dfs(nr,nc,i,j,v)
-        
-#         for i in range(m):
-#             for j in range(n):
-#                 if grid[i][j] ==1:
-#                     v=[]
-#                     dfs(i,j,i,j,v)
-#                     ans.add(tuple(v))
-            
-#         return len(ans)
-
 class Solution:
     def countDistinctIslands(self, grid : List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
         
         def valid(i,j):
-            return (i>=0 and i<m and j>=0 and j<n and grid[i][j] == 1)
+            if i>=0 and i<m and j>=0 and j<n and vis[i][j] == 0 and grid[i][j] == 1:
+                return True
+            return False
             
         row=[-1,0,1,0]
         col=[0,1,0,-1]
+        vis=[[0 for _ in range(n)] for _ in range(m)]
         ans=set()
         
         def dfs(i,j,br,bc,v):
-            grid[i][j] = 0
+            vis[i][j] = 1
             v.append((i-br,j-bc))
             
             for x in range(4):
@@ -62,10 +32,10 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j] ==1:
+                if vis[i][j]==0 and grid[i][j] ==1:
                     v=[]
                     dfs(i,j,i,j,v)
-                    
+                    # print(tuple(v))
                     ans.add(tuple(v))
         return len(ans)
         
